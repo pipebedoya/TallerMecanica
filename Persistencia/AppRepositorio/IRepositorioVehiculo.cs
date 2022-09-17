80b1fc83ee08a07fb0feff760a73c85e7c15bca0
Juanjaramillo71 <j.djaramillo@hotmail.com>
2022-09-16 22:58:01 -0500

    update

--- Persistencia/AppRepositorio/IRepositorioVehiculo.cs
+++ Persistencia/AppRepositorio/IRepositorioVehiculo.cs
@@ -1,11 +1,20 @@
 using System.Collections.Generic;
+/*using System.Collections.Generic;*/
 using Dominio;
-
 
 namespace TallerMecanica.Persistencia
 {
     public interface IRepositorioVehiculo
     {
+    
+       /* IEnumerable<Vehiculo> GetAllVehiculos();
+        Vehiculo AddVehiculo(vehiculo vehiculo);
+        VehiculoUpdateVehiculo(Vehicuo Vehiculo);
+        void DeleteVehiculo(int Id_Vehiculo);
+        vehiculo GetVehiculo(int Id_Vehiculo);*/
+
+
+
        /* IEnumerable<Vehiculo> GetAllVehiculos();
         Vehiculo AddVehiculo(Vehiculo Vehiculo);
         Vehiculo UpdateVehiculo(Vehiculo Vehiculo);
--- Persistencia/AppRepositorio/RepositorioVehiculo.cs
+++ Persistencia/AppRepositorio/RepositorioVehiculo.cs
@@ -1,13 +1,34 @@
+using System;
+/*using IRepositorioVehiculo;*/
+
 namespace TallerMecanica.Persistencia
 {
     public class RepositorioVehiculo : IRepositorioVehiculo
     {
-        /*IEnumerable<Vehiculo>.GetAllVehiculos();
-        Vehiculo AddVehiculo(Vehiculo Vehiculo);
-        Vehiculo UpdateVehiculo(Vehiculo Vehiculo);
-        void DeleteVehiculo(int Id_Vehiculo );
-        Vehiculo GetVehiculo(int Id_Vehiculo);*/
+        /*private readonly AppContext _appContext;
+
+        public RepositorioVehiculo(AppContext appContext)
+        {
+               _appContext = appContext;
+        }
         
+        IEnumerable<Vehiculo> IRepositorioVehiculo.GetAllVehiculos();
+        {
+            throw new System.NotimplementedException();
+        }
+        Vehiculo IRepositorioVehiculo.AddVehiculo(Vehiculo Vehiculo);
+        {
+            
+        }
+
+
+        Vehiculo IRepositorioVehiculo.UpdateVehiculo(Vehiculo Vehiculo);
+        {throw new System.NotimplementedException();}
+        void IRepositorio.DeleteVehiculo(int Id_Vehiculo );
+        {throw new System.NotimplementedException();}
+        Vehiculo IRepositorio.GetVehiculo(int Id_Vehiculo);
+        {throw new System.NotimplementedException();}
+        */
 
     }
 
